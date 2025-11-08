2ea1a193650f737180979094dfcc73514fb46753
Ikram Yahya <ekrammohammed330@gmail.com>
2025-11-07 19:57:21 -0600

    Add PieceType.java enum listing all chess piece types

---
+++ src/chessgui/PieceType.java
@@ -0,0 +1,10 @@
+package chessgui;
+
+import java.io.Serializable;
+
+/**
+ * Type of chess piece.
+ */
+public enum PieceType implements Serializable {
+    PAWN, ROOK, KNIGHT, BISHOP, QUEEN, KING
+}
