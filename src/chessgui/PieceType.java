68404144b7c0b6c3c2c6bfe1c0fd9f61fca40b85
Ikram Yahya <ekrammohammed330@gmail.com>
2025-12-01 00:32:02 -0600

    Add PieceType enum: defines pawn, rook, knight, bishop, queen, king for GUI piece handling

--- src/chessgui/PieceType.java
+++ src/chessgui/PieceType.java
@@ -2,9 +2,6 @@ package chessgui;
 
 import java.io.Serializable;
 
-/**
- * Type of chess piece.
- */
 public enum PieceType implements Serializable {
     PAWN, ROOK, KNIGHT, BISHOP, QUEEN, KING
 }
